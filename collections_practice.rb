b0d3543b1e0116a3fda66ac0a9c7880c13d21b0a
danielglatstein <danielglatstein@gmail.com>
2015-06-24 10:25:28 -0400

    final answers

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,4 @@
 def begins_with_r(array)
-<<<<<<< HEAD
  #Return true if every element of the tools array starts with an "r" 
  #and false otherwise.
  array.each do |string|
@@ -41,16 +40,7 @@ def remove_non_strings(array)
      string_array << element
    end
  end
-=======
-	#Return true if every element of the tools array starts with an "r" 
-	#and false otherwise.
-	array.each do |string|
-		if string.start_with?("r") != true
-			return false
-		end
-	end
-	true
-end
+
 
 def contain_a(array)
 	#return all elements that contain the letter 'a'
@@ -74,6 +64,7 @@ def first_wa(array)
 	end
 	false
 end
+
 def remove_non_strings(array)
 	#remove anything that's not a string from an array
 	string_array = [ ]
@@ -82,12 +73,10 @@ def remove_non_strings(array)
 			string_array << element
 		end
 	end
->>>>>>> 1c58917c98b627fa76571fe095fa733424473406
     string_array
 end
 
 def count_elements (array)
-<<<<<<< HEAD
  #count how many times something appears in an array
   final_count = [ ]
 
@@ -155,7 +144,8 @@ def organize_schools(schools)
  sorted_hash
 end 
 
-=======
+
+def count_elements(array)
 	#count how many times something appears in an array
 	name_count = Hash.new 
 	count = [ ]
@@ -171,15 +161,52 @@ end
 	name_count
 end
 
-def merge_data
-	#combines two nested data structures into one
+def merge_data(keys, data)
+ #combines two nested data structures into one
+ new_array = [ ]
+ keys.each do |k|
+  x = k[:first_name]
+    data.each do |d|
+      if d.has_key?(x)
+        new_hash = {:first_name=>x, :awesomeness=>d[x][:awesomeness], :height=>d[x][:height], :last_name=>d[x][:last_name]}
+          new_array << new_hash
+      end
+    end
+ end
+ new_array
 end
 
-def find_cool
-	#find all cool hashes
+def find_cool(data)
+ #find all cool hashes
+ new_array = [ ]
+ data.each do |name|
+  if name[:temperature] == "cool"
+    new_array << name
+  end
+ end
+ new_array
 end
 
-def organize_schools
-	# organizes the schools by location
-end
->>>>>>> 1c58917c98b627fa76571fe095fa733424473406
+def organize_schools(schools)
+ # organizes the schools by location
+ sorted_hash = Hash.new
+
+ cities = [ ]
+ 
+ schools.each do |school|
+  cities << school[1][:location]
+ end
+ 
+ cities = cities.uniq
+ 
+ cities.each do |city|
+    sorted_hash[city] = [ ]
+    schools.each do |school|
+        if city == school[1][:location]
+          sorted_hash[city] << school[0]
+        end
+    end
+ end
+ sorted_hash
+end 
+

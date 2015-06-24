d2ef472524a92279c932223b60a28c05551749ab
danielglatstein <danielglatstein@gmail.com>
2015-06-24 10:28:33 -0400

    update syntax errors

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,7 @@ def remove_non_strings(array)
      string_array << element
    end
  end
-
+end
 
 def contain_a(array)
 	#return all elements that contain the letter 'a'
@@ -110,6 +110,8 @@ def merge_data(keys, data)
  new_array
 end
 
+
+
 def find_cool(data)
  #find all cool hashes
  new_array = [ ]
@@ -133,80 +135,15 @@ def organize_schools(schools)
  
  cities = cities.uniq
  
- cities.each do |city|
-    sorted_hash[city] = [ ]
-    schools.each do |school|
-        if city == school[1][:location]
-          sorted_hash[city] << school[0]
-        end
-    end
- end
- sorted_hash
-end 
-
-
-def count_elements(array)
-	#count how many times something appears in an array
-	name_count = Hash.new 
-	count = [ ]
-	array.each do |name_set|
-    	name_set.each do |name|
-    		if name_count.has_key?(name)
-    			name_count[name] = name_count[name] + 1
-    		else
-    			name_count[name] = 1
-    		end
-    	end
-	end
-	name_count
-end
-
-def merge_data(keys, data)
- #combines two nested data structures into one
- new_array = [ ]
- keys.each do |k|
-  x = k[:first_name]
-    data.each do |d|
-      if d.has_key?(x)
-        new_hash = {:first_name=>x, :awesomeness=>d[x][:awesomeness], :height=>d[x][:height], :last_name=>d[x][:last_name]}
-          new_array << new_hash
+   cities.each do |city|
+      sorted_hash[city] = [ ]
+      schools.each do |school|
+          if city == school[1][:location]
+            sorted_hash[city] << school[0]
+          end
       end
-    end
- end
- new_array
-end
-
-def find_cool(data)
- #find all cool hashes
- new_array = [ ]
- data.each do |name|
-  if name[:temperature] == "cool"
-    new_array << name
-  end
- end
- new_array
-end
-
-def organize_schools(schools)
- # organizes the schools by location
- sorted_hash = Hash.new
-
- cities = [ ]
- 
- schools.each do |school|
-  cities << school[1][:location]
- end
- 
- cities = cities.uniq
- 
- cities.each do |city|
-    sorted_hash[city] = [ ]
-    schools.each do |school|
-        if city == school[1][:location]
-          sorted_hash[city] << school[0]
-        end
-    end
- end
+   end
  sorted_hash
 end 
+
 
